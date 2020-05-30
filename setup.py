a3cc0590cd4e4d737662062acaf468a9d5ed9e25
Roi Levi <51533978+r0eilevi@users.noreply.github.com>
2020-05-30 20:21:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,20 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+	long_description = f.read()
+
+with open('requirements.txt') as f:
+	requirements = f.read().splitlines()
+
 setup(
-  name = 'coroapi',
-  packages = ['coroapi'],
-  version = '1.00',
-  license='MIT',
-  description = 'Fast, and Up-to-Date Corona Virus API with up to 100 supported countries',
-  author = 'Roi Levi',
-  author_email = 'roeil4939@gmail.com',
-  url = 'https://github.com/r0eilevi/coronavirus-api',
-  download_url = 'https://github.com/r0eilevi/coronavirus-api/archive/v1.00.tar.gz', # change this
-  keywords = ['api', 'corona', 'virus', 'fast', 'covid19', 'up-to-date'],
-  install_requires=[
-          'requests',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+	name='coroapi',
+	version='1.00',
+	author='Roi Levi',
+	author_email='roeil4939@gmail.com',
+	description='coroapi Is fast and up-to-date corona virus news in the world, support up to 100 countries',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/r0eilevi/coronavirus-api',
+	packages=find_packages(),
+	install_requires=requirements
 )
