75bcd5fd03d237fc5d838fc31b0f997e7b5f401f
Roi Levi <51533978+r0eilevi@users.noreply.github.com>
2020-05-30 20:32:15 +0300

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -1,1 +1,15 @@
+# All results is right to today (5/30/2020)
+# list of supported countries can be printed out | print(instance.countries)
 
+import coroapi
+
+instance = Corona()
+usa_infected_people = instance.get_country_infected('usa', text=False)
+print(usa_infected_people)
+
+<< 1802086
+
+israel_total_deathes = instance.get_country_deathes('israel', text=True)
+print(israel_total_deathes)
+
+<< Deaths: 284
