67a06f3e43c1544e60a8f278c3393e661456e7d9
Roi Levi <51533978+r0eilevi@users.noreply.github.com>
2020-05-30 20:39:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,5 +16,8 @@ setup(
 	long_description_content_type='text/markdown',
 	url='https://github.com/r0eilevi/coronavirus-api',
 	packages=find_packages(),
-	install_requires=requirements
+	install_requires=[
+          'requests',
+          'beautifulsoup4',
+      ]
 )
