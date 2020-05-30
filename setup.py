fb312f70aea71319db4a3ec590a3ba37f2bbda1b
Roi Levi <51533978+r0eilevi@users.noreply.github.com>
2020-05-30 20:16:45 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'coroapi',
+  packages = ['coroapi'],
+  version = '1.00',
+  license='MIT',
+  description = 'Fast, and Up-to-Date Corona Virus API with up to 100 supported countries',
+  author = 'Roi Levi',
+  author_email = 'roeil4939@gmail.com',
+  url = 'https://github.com/r0eilevi/coronavirus-api',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz', # change this
+  keywords = ['api', 'corona', 'virus', 'fast', 'covid19', 'up-to-date'],
+  install_requires=[
+          'requests',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
