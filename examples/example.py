6f5241c4aba59e59ccbba530ac7bfbf301a56333
Roi Levi <51533978+r0eilevi@users.noreply.github.com>
2020-05-31 17:51:23 +0300

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -1,3 +1,4 @@
+# All results is right to today (5/30/2020)
 # list of supported countries can be printed out | print(instance.countries)
 
 import coroapi
@@ -8,3 +9,6 @@ print(usa_infected_people)
 
 israel_total_deathes = instance.get_country_deathes('israel', text=True)
 print(israel_total_deathes)
+
+highest_country_cases = instance.get_rank_by_country('usa', text=False)
+print(highest_country_cases)
