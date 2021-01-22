c7e4c7d22754ea8ae912ff39ffaf77447bd6302d
r0ei <roeil4939@gmail.com>
2021-01-22 12:43:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open('README.md', 'r') as f:
 	long_description = f.read()
 	
 setup(
-	name='coroapi_corona',
-	version='2.0',
+	name='coro_api',
+	version='1.0',
 	author='Roi Levi',
 	author_email='roeil4939@gmail.com',
 	description='coroapi is fast and up-to-date Covid-19 API',
@@ -13,7 +13,7 @@ setup(
 	long_description_content_type='text/markdown',
 	url='https://github.com/r0ei/coroapi',
 	packages=find_packages(),
-	data_files=[('coroapi', ['coroapi/*.json'])],
+	package_data={'.': ['data/countries.json']},
 	include_package_data=True,
 	install_requires=[
         'requests',
