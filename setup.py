9fc2c0cc56f84bdeb73a281ba759f9e68320ad55
r0ei <roeil4939@gmail.com>
2021-01-23 10:34:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
 	long_description_content_type='text/markdown',
 	url='https://github.com/r0ei/coroapi',
 	packages=find_packages(),
-	package_data={'coroapi': ['countries.json']},
+	package_data={'.': ['countries.json']},
 	include_package_data=True,
 	install_requires=[
         'requests',
